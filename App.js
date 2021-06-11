33d38d3d720dd7031241b73cd81b72bbc1be6a5b
sn-007 <swamynaidu.ch@research.iiit.ac.in>
2021-06-12 02:27:12 +0530

    login page UI completed

--- App.js
+++ App.js
@@ -10,11 +10,16 @@ import homeScreen from './screens/homeScreen'
 
 
 const Stack = createStackNavigator();
+const topBarOptions ={
+  headerStyle :{backgroundColor:"#2c6BED"},
+  headerTitleStyle:{color:"white"},
+  headerTintColor:"white"
+}
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={topBarOptions}>
         <Stack.Screen name="Login" component={loginScreen} />
         <Stack.Screen name="Register" component={registerScreen} />
         <Stack.Screen name="Home" component={homeScreen} />
--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -1,21 +1,63 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image, KeyboardAvoidingView } from 'react-native';
+import { Input, Button } from 'react-native-elements';
+import {StatusBar} from "expo-status-bar"
+
+
 
 const loginScreen = () => {
 
-    return (
-        <View style={styles.container}> 
-        <Text>Hi this is loginScreen</Text>
-        </View>
-    );
+  return (
+    <KeyboardAvoidingView behavior='padding' style={styles.container}>
+      <StatusBar style="auto"/>
+      <Image style={styles.logo} source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/5/56/Logo_Signal..png' }} />
+
+      <View style={styles.input}>
+        <Input
+          placeholder="Email"
+          type="email"
+          autofocus
+          leftIcon={{ type: 'Feather', name: 'mail' }}
+        />
+
+        <Input
+          placeholder="Password"
+          type="password"
+          leftIcon={{ type: 'EvilIcons', name: 'lock' }}
+          secureTextEntry={true}
+          
+        />
+      </View>
+      <Button title="Login" containerStyle={styles.button}/>
+      <Button title="Register" containerStyle={styles.button} type="outline"/>
+
+
+    </KeyboardAvoidingView>
+
+  );
 
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex:1,
-      paddingTop: 30
-      
-    },
-  });
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: 15
+  },
+
+  logo: {
+    height:200,
+    width: 200,
+  },
+
+  input: {
+    width:300,
+    paddingBottom:5
+  },
+
+  button:{
+    width:200,
+    paddingBottom:10,
+  }
+});
 export default loginScreen;
